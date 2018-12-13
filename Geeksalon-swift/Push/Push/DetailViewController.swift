becbc412f1a346ac5cdaf89049528fd2ea8dd631
shotaro-ooe <shoooe97@gmail.com>
2018-12-13 17:20:32 +0900

    Pushの使い方

---
+++ Geeksalon-swift/Push/Push/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Push
+//
+//  Created by 大江祥太郎 on 2018/12/13.
+//  Copyright © 2018年 shotaro. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Geeksalon-swift/Push/Push/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Push
+//
+//  Created by 大江祥太郎 on 2018/12/13.
+//  Copyright © 2018年 shotaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
